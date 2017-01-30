be39944e740a7d2b07cdd1455d5825943b19f37d
Chuan Miao <chuan137@gmail.com>
2017-01-30 11:10:07 +0100

    Add provider info to signup middleware

--- src/middleware/signup.js
+++ src/middleware/signup.js
@@ -3,12 +3,14 @@
 module.exports = function(app) {
   return function(req, res, next) {
     app.service('users').create({
-      email: req.body.email,
-      password: req.body.password
-    })
-    .then(user => {
-      res.send(user);
-    })
-    .catch(next);
+        email: req.body.email,
+        password: req.body.password
+      }, {
+        provider: 'server'
+      })
+      .then(user => {
+        res.send(user);
+      })
+      .catch(next);
   };
 };
