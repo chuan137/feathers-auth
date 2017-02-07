7696062ad6cadb631eace786e0ec4b7d0e56a5c6
Chuan Miao <chuan137@gmail.com>
2017-02-08 00:50:58 +0100

    Use conditional hook to add user's role.

--- src/services/user/hooks/addDefaultRole.js
+++ src/services/user/hooks/addDefaultRole.js
@@ -1,13 +1,16 @@
 const checkContext = require('feathers-hooks-common/lib/utils').checkContext;
 const getByDot = require('feathers-hooks-common/lib/utils').getByDot;
 const setByDot = require('feathers-hooks-common/lib/utils').setByDot;
+const debug = require('debug')('users:hooks:defaultRole');
 
 module.exports = function(role) {
   return (hook) => {
     checkContext(hook, 'before', ['create']);
-    let roles = getByDot(hook, 'data.roles');
-    if (roles === undefined || roles === []) {
-      setByDot(hook, 'data.roles', [role]);
+    let roles = hook.data.roles;
+    if (roles instanceof Array) {
+      hook.data.roles = roles.concat([role]);
+    } else {
+      hook.data.roles = [role];
     }
     return Promise.resolve(hook);
   };
--- src/services/user/hooks/index.js
+++ src/services/user/hooks/index.js
@@ -1,19 +1,29 @@
 'use strict';
 
+const _ = require('lodash');
 const globalHooks = require('../../../hooks');
 const hooks = require('feathers-hooks-common');
 const auth = require('feathers-authentication');
 const local = require('feathers-authentication-local');
 const verifyHooks = require('feathers-authentication-management').hooks;
-const defaultRole = require('./defaultRole');
+const addDefaultRole = require('./addDefaultRole');
+
+const isRoleSet = (hook) =>
+  hook.data.roles && hook.data.roles instanceof Array && hook.data.roles.length > 0;
+const isNotRoleSet = (hook) => !isRoleSet(hook);
+const isAdmin = (hook) =>
+  hook.data.roles && hook.data.roles instanceof Array && _.includes(hook.data.roles, 'admin');
+const isNotAdmin = (hook) => !isAdmin(hook);
 
 exports.before = {
   find: [
     auth.hooks.authenticate(['jwt'])
   ],
   create: [
-    local.hooks.hashPassword({ passwordField: 'password' }),
-    defaultRole('user'),
+    local.hooks.hashPassword({
+      passwordField: 'password'
+    }),
+    hooks.iff(isNotRoleSet, addDefaultRole('user')),
     verifyHooks.addVerification()
   ]
 };
@@ -22,9 +32,9 @@ exports.after = {
   all: [hooks.remove('password')],
   find: [],
   get: [],
-  create: [ 
+  create: [
     verifyHooks.removeVerification(), // when and what to remove
-    globalHooks.sendVerifySignup()
+    hooks.iff(isNotAdmin, globalHooks.sendVerifySignup())
   ],
   update: [],
   patch: [],
